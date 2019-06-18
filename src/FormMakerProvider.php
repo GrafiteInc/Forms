2059cbcd9f9da378f8b854cf200498a8a4cb5d1c
Matt Lantz <mattlantz@gmail.com>
2019-06-18 14:15:33 -0400

    5.8 Updates

--- src/FormMakerProvider.php
+++ src/FormMakerProvider.php
@@ -20,6 +20,33 @@ class FormMakerProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/form-maker.php' => base_path('config/form-maker.php'),
         ]);
+
+        /*
+        |--------------------------------------------------------------------------
+        | Blade Directives
+        |--------------------------------------------------------------------------
+        */
+
+        Blade::directive('form_maker_table', function ($expression) {
+            return "<?php echo FormMaker::fromTable($expression); ?>";
+        });
+         Blade::directive('form_maker_array', function ($expression) {
+            return "<?php echo FormMaker::fromArray($expression); ?>";
+        });
+         Blade::directive('form_maker_object', function ($expression) {
+            return "<?php echo FormMaker::fromObject($expression); ?>";
+        });
+         Blade::directive('form_maker_columns', function ($expression) {
+            return "<?php echo FormMaker::getTableColumns($expression); ?>";
+        });
+
+        // Label Maker
+        Blade::directive('input_maker_label', function ($expression) {
+            return "<?php echo InputMaker::label($expression); ?>";
+        });
+         Blade::directive('input_maker_create', function ($expression) {
+            return "<?php echo InputMaker::create($expression); ?>";
+        });
     }
 
     /**
