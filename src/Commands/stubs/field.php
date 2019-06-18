59ec154b5949342791ff5a454f349d371c6a5584
Matt Lantz <mattlantz@gmail.com>
2019-06-18 12:36:42 -0400

    Remove facades

---
+++ src/Commands/stubs/field.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Grafite\FormMaker\Fields;
+
+use Grafite\FormMaker\Fields\Field;
+
+class {field} extends Field
+{
+    /**
+     * Input type
+     *
+     * @return string
+     */
+    protected static function getType()
+    {
+        return 'text';
+    }
+
+    /**
+     * Input attributes
+     *
+     * @return array
+     */
+    protected static function getAttributes()
+    {
+        return [];
+    }
+
+    /**
+     * Field maker options
+     *
+     * @return array
+     */
+    protected static function getOptions()
+    {
+        return [];
+    }
+
+    /**
+     * View path for custom templates
+     *
+     * @return string
+     */
+    protected static function getView()
+    {
+        return null;
+    }
+}
---
+++ src/Commands/stubs/form.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Forms;
+
+use Grafite\FormMaker\Forms\ModelForm;
+
+class {form} extends ModelForm
+{
+    public $model = {model}::class;
+
+    public $routePrefix = '{prefix}';
+
+    public function fields()
+    {
+        return [
+            //
+        ];
+    }
+}
