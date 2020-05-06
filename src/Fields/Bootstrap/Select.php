573263b6bdaefd5fc0f75df8adf7e3b4b384d815
Matt Lantz <mattlantz@gmail.com>
2020-05-06 08:31:05 -0400

    Because this select is better than browser default

---
+++ src/Fields/Bootstrap/Select.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Grafite\FormMaker\Fields\Bootstrap;
+
+use Grafite\FormMaker\Fields\Field;
+
+class Select extends Field
+{
+    protected static function getType()
+    {
+        return 'select';
+    }
+
+    protected static function getAttributes()
+    {
+        return [
+            'class' => 'selectpicker',
+            'multiple' => true,
+        ];
+    }
+
+    protected static function getFactory()
+    {
+        return 'text(50)';
+    }
+
+    protected static function stylesheets($options)
+    {
+        return [
+            "//cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css",
+        ];
+    }
+
+    protected static function scripts($options)
+    {
+        return [
+            '//cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js'
+        ];
+    }
+
+    protected static function js($id, $options)
+    {
+        $btn = $options['btn'] ?? 'btn-primary';
+
+        return <<<EOT
+$('.selectpicker').selectpicker({
+    style: "{$btn}",
+}).parent().css({
+    display: "block",
+    width: "100%"
+});
+EOT;
+    }
+}
