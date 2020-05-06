9405b597f7dc30ffe7a56c546484a3abbeb92a0e
Matt Lantz <mattlantz@gmail.com>
2020-05-06 08:29:50 -0400

    Because slugs are more common than it seems

---
+++ src/Fields/Slug.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Grafite\FormMaker\Fields;
+
+use Grafite\FormMaker\Fields\Field;
+
+class Slug extends Field
+{
+    protected static function getType()
+    {
+        return 'text';
+    }
+
+    protected static function getFactory()
+    {
+        return 'text(50)';
+    }
+
+    protected static function js($id, $options)
+    {
+        return <<<EOT
+document.getElementById('$id')
+.addEventListener("keyup", event => {
+    event.preventDefault();
+    let str = document.getElementById('$id').value;
+    str = str.replace(/\W+(?!$)/g, '-').toLowerCase();
+    document.getElementById('$id').value = str;
+});
+EOT;
+    }
+}
