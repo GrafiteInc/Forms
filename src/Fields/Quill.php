7f727a7c2a2c9a3b6c14cdba3772af708c169666
Matt Lantz <mattlantz@gmail.com>
2020-08-12 08:55:49 -0400

    Because Quill was completely broken

--- src/Fields/Quill.php
+++ src/Fields/Quill.php
@@ -6,9 +6,18 @@ use Grafite\FormMaker\Fields\Field;
 
 class Quill extends Field
 {
+    protected static function fieldOptions()
+    {
+        return [
+            'theme',
+            'quill_theme',
+            'toolbars',
+        ];
+    }
+
     protected static function getType()
     {
-        return 'div';
+        return 'hidden';
     }
 
     protected static function getAttributes()
@@ -31,20 +40,120 @@ class Quill extends Field
         ];
     }
 
+    protected static function styles($id, $options)
+    {
+        $theme = $options['theme'] ?? 'light';
+        $darkTheme = '';
+
+        if ($theme == 'dark') {
+            $darkTheme = <<<EOT
+    .ql-container.ql-snow {
+        border: 1px solid #111;
+    }
+
+    .ql-toolbar.ql-snow {
+        border: 1px solid #000;
+        background-color: #000;
+    }
+
+    .ql-editor {
+        background-color: #111;
+    }
+EOT;
+        }
+
+        return <<<EOT
+    .ql-container {
+        font-size: 16px;
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+
+    .ql-toolbar.ql-snow {
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+    }
+
+    .ql-editor {
+        padding: 24px;
+    }
+
+    .ql-snow .ql-color-picker .ql-picker-label svg, .ql-snow .ql-icon-picker .ql-picker-label svg {
+        vertical-align: top;
+    }
+
+    {$darkTheme}
+EOT;
+    }
+
     protected static function scripts($options)
     {
         return ['//cdn.quilljs.com/1.3.6/quill.js'];
     }
 
+    protected static function getTemplate()
+    {
+        return <<<EOT
+<div class="{rowClass}">
+    <label for="{id}" class="{labelClass}">{name}</label>
+    <div class="{fieldClass}">
+        <div id="{id}_Editor"></div>
+        {field}
+        {errors}
+    </div>
+</div>
+EOT;
+    }
+
     protected static function js($id, $options)
     {
-        $theme = $options['theme'] ?? 'snow';
+        $theme = $options['quill_theme'] ?? 'snow';
         $placeholder = $options['placeholder'] ?? '';
+        $toolbars = $options['toolbars'] ?? [
+            'basic',
+            'extra',
+            'lists',
+            'super_sub',
+            'indents',
+            'headers',
+            'colors',
+        ];
+
+        $toolbars = collect($toolbars);
+
+        throw_if ($toolbars->isEmpty(), new \Exception('You cannot have an empty toolbar.'));
+
+        $basic = ($toolbars->contains('basic')) ? "['bold', 'italic', 'underline', 'strike', { 'align': [] }]" : '';
+        $extra = ($toolbars->contains('extra')) ? "['blockquote', 'code-block']" : '';
+        $lists = ($toolbars->contains('lists')) ? "[{ 'list': 'ordered'}, { 'list': 'bullet' }]" : '';
+        $superSub = ($toolbars->contains('super_sub')) ? "[{ 'script': 'sub'}, { 'script': 'super' }]" : '';
+        $indents = ($toolbars->contains('indents')) ? "[{ 'indent': '-1'}, { 'indent': '+1' }]" : '';
+        $headers = ($toolbars->contains('headers')) ? "[{ 'header': [1, 2, 3, 4, 5, 6, false] }]" : '';
+        $colors = ($toolbars->contains('colors')) ? "[{ 'color': [] }, { 'background': [] }]" : '';
 
         return <<<EOT
-new Quill('#$id', {
+var _editor_{$id}_toolbarOptions = [
+    {$basic},
+    {$extra},
+    {$lists},
+    {$superSub},
+    {$indents},
+    {$headers},
+    {$colors},
+    ['clean']
+];
+
+var {$id}_Quill = new Quill('#{$id}_Editor', {
     theme: '$theme',
-    placeholder: '$placeholder'
+    placeholder: '$placeholder',
+    modules: {
+        toolbar: _editor_{$id}_toolbarOptions
+    }
+});
+
+document.getElementById('{$id}_Editor').firstChild.innerHTML = document.getElementById('{$id}').value;
+document.getElementById('{$id}_Editor').addEventListener('keydown', function () {
+    document.getElementById('{$id}').value = document.getElementById('{$id}_Editor').firstChild.innerHTML;
 });
 EOT;
     }
