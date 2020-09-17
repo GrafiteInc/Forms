0de7d3e264b9d46e94940416c9d4179c458a80c6
Matt Lantz <mattlantz@gmail.com>
2020-09-17 18:18:58 -0400

    Because sometimes a span is all you need

---
+++ src/Html/Span.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Grafite\Forms\Html;
+
+use Grafite\Forms\Html\HtmlSnippet;
+
+class Span extends HtmlSnippet
+{
+    public static function content($options = [])
+    {
+        $class = '';
+        $content = '';
+
+        if (isset($options['class'])) {
+            $class = " class=\"{$options['class']}\"";
+        }
+
+        if (isset($options['content'])) {
+            $content = $options['content'];
+        }
+
+        throw_if(empty($content), 'You cannot have an empty heading');
+
+        return "<span{$class}>{$content}</span>";
+    }
+}
