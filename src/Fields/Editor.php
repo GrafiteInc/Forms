46fa555473a8e2c17a5cfe501e7e77aaf68fc536
Matt Lantz <mattlantz@gmail.com>
2021-02-15 14:44:46 -0500

    Because editorjs is quite powerful

---
+++ src/Fields/Editor.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Grafite\Forms\Fields;
+
+class Editor extends Field
+{
+    protected static function getType()
+    {
+        return 'hidden';
+    }
+
+    protected static function getAttributes()
+    {
+        return [];
+    }
+
+    protected static function getFactory()
+    {
+        return 'string';
+    }
+
+    protected static function getTemplate()
+    {
+        return <<<EOT
+        <div class="{rowClass}">
+            <label for="{id}" class="{labelClass}">{name}</label>
+            <div class="{fieldClass}">
+                <div id="Editor_{id}" class="editor_js_container"></div>
+                {field}
+                {errors}
+            </div>
+        </div>
+EOT;
+    }
+
+    protected static function styles($id, $options)
+    {
+        $theme = $options['theme'] ?? 'light';
+
+        $borderColor = 'ced4da';
+        $backgroundColor = 'FFF';
+        $borderThickness = '1';
+
+        if ($theme === 'dark') {
+            $borderColor = '333';
+            $backgroundColor = '1F1F1F';
+            $borderThickness = '2';
+        }
+
+        return <<<EOT
+.editor_js_container {
+    border-radius: 4px;
+    border: ${borderThickness}px solid #${borderColor};
+    background-color: #${backgroundColor};
+    padding: 24px;
+    width: 100%;
+}
+EOT;
+    }
+
+    protected static function scripts($options)
+    {
+        return [
+            '//cdn.jsdelivr.net/npm/@editorjs/editorjs@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/header@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/paragraph@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/link@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/underline@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/table@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/quote@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/checklist@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/marker@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/inline-code@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/code@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/list@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/image@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/embed@latest',
+            '//cdn.jsdelivr.net/npm/@editorjs/delimiter@latest',
+        ];
+    }
+
+    protected static function js($id, $options)
+    {
+        $fileUploadRoute = $options['fileUploadRoute'];
+        $placeholder = $options['placeholder'];
+
+        return <<<EOT
+const editor_{$id} = new EditorJS({
+    holder: 'Editor_{$id}',
+    placeholder: '{$placeholder}',
+    data: JSON.parse(document.getElementById('{$id}').value),
+    tools: {
+        header: Header,
+        delimiter: Delimiter,
+        paragraph: {
+            class: Paragraph,
+            inlineToolbar: true,
+        },
+        list: {
+            class: List,
+            inlineToolbar: true,
+        },
+        embed: Embed,
+        code: CodeTool,
+        image: {
+            class: ImageTool,
+            config: {
+                additionalRequestHeaders: {
+                    "X-CSRF-TOKEN": document.head.querySelector('meta[name="csrf-token"]').content
+                },
+                endpoints: {
+                    byFile: '{$fileUploadRoute}',
+                }
+            }
+        },
+        underline: Underline,
+        table: Table,
+        quote: Quote,
+        checklist: Checklist,
+        marker: Marker,
+        inlineCode: InlineCode,
+    },
+});
+
+let _form = document.getElementById('{$id}').form;
+
+_form.addEventListener('submit', function () {
+    editor_{$id}.save().then((outputData) => {
+        document.getElementById('{$id}').value = JSON.stringify(outputData);
+    }).catch((error) => {
+        console.log('Saving failed: ', error)
+    });
+
+    return true;
+});
+
+EOT;
+    }
+}
