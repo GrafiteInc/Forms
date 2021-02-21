eb31163861ff4a0f0d4b0ed4c170b880291eafa0
Matt Lantz <mattlantz@gmail.com>
2021-02-21 15:08:38 -0500

    Because of file names

--- src/Fields/CustomFile.php
+++ src/Fields/CustomFile.php
@@ -16,8 +16,37 @@ class CustomFile extends Field
         return [];
     }
 
+    protected static function getAttributes()
+    {
+        return [
+            'onChange' => 'window.FormMaker_customFile(this);'
+        ];
+    }
+
     protected static function getFactory()
     {
         return 'image';
+    }
+
+    protected static function getTemplate()
+    {
+        return <<<EOT
+<div class="{rowClass}">
+    <label for="{id}" class="{labelClass}">{name}</label>
+    <div class="{fieldClass}">
+        {field}
+    </div>
+    {errors}
+</div>
+EOT;
+    }
+
+    protected static function js($id, $options)
+    {
+        return <<<EOT
+window.FormMaker_customFile = function (input) {
+    input.nextElementSibling.innerHTML = input.files[0].name;
+}
+EOT;
     }
 }
--- src/Fields/FileWithPreview.php
+++ src/Fields/FileWithPreview.php
@@ -51,6 +51,7 @@ EOT;
 
         return <<<EOT
 window.FormMaker_previewFileUpload = function (input) {
+    input.nextElementSibling.innerHTML = input.files[0].name;
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
