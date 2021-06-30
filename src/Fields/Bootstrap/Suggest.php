bc80b962087d599b09815d4dfe0f4f5422a096b9
Matt Lantz <mattlantz@gmail.com>
2021-06-30 10:34:03 -0400

    because this is no longer needed

--- src/Fields/Bootstrap/Suggest.php
+++ src/Fields/Bootstrap/Suggest.php
@@ -20,37 +20,4 @@ class Suggest extends Select
             'data-size' => 8,
         ];
     }
-
-    protected static function getFactory()
-    {
-        return 'text(50)';
-    }
-
-    protected static function stylesheets($options)
-    {
-        return [
-            "//cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css",
-        ];
-    }
-
-    protected static function scripts($options)
-    {
-        return [
-            '//cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js'
-        ];
-    }
-
-    protected static function js($id, $options)
-    {
-        $btn = $options['btn'] ?? 'btn-outline-primary';
-
-        return <<<EOT
-$('#${id}').selectpicker({
-    style: "{$btn}"
-}).parent().css({
-    display: "block",
-    width: "100%"
-});
-EOT;
-    }
 }
