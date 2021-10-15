53ac10abc8071aa1ebffdce26c9fd36b3874ac5c
Matt Lantz <mattlantz@gmail.com>
2021-10-15 08:39:18 -0400

    because of v6 changes

--- src/Html/HoneyPot.php
+++ src/Html/HoneyPot.php
@@ -2,25 +2,8 @@
 
 namespace Grafite\Forms\Html;
 
-use Illuminate\Support\Str;
-use Grafite\Forms\Services\HtmlConfigProcessor;
-
 class HoneyPot extends HtmlSnippet
 {
-    public static function make($content = null, $name = null)
-    {
-        $options = [
-            'type' => 'html',
-            'content' => null,
-        ];
-
-        if (is_null($name)) {
-            $name = 'honeypot';
-        }
-
-        return (new HtmlConfigProcessor($name, $options));
-    }
-
     public static function render($options = [])
     {
         return view('honeypot::honeypotFormFields');
