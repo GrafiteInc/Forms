2513974ca06cc8b844190560b5d650dbcaf585bd
Matt Lantz <mattlantz@gmail.com>
2022-04-01 11:54:39 -0400

    because of rating fields

---
+++ src/Fields/Rating.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Grafite\Forms\Fields;
+
+use Grafite\Forms\Fields\Field;
+
+class Rating extends Field
+{
+    protected static function getType()
+    {
+        return 'select';
+    }
+
+    protected static function getFactory()
+    {
+        return 'text(50)';
+    }
+
+    public static function stylesheets($options)
+    {
+        return [
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/fontawesome-stars.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/fontawesome-stars-o.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/css-stars.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-pill.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-movie.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-square.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-1to10.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-reversed.css',
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/dist/themes/bars-horizontal.css',
+        ];
+    }
+
+    public static function scripts($options)
+    {
+        return [
+            '//cdn.jsdelivr.net/npm/jquery-bar-rating@1.2.2/jquery.barrating.min.js',
+        ];
+    }
+
+    public static function js($id, $options)
+    {
+        $theme = $options['theme'] ?? 'fontawesome-stars-o';
+
+        return <<<JS
+            $(function() {
+                $('#{$id}').barrating({
+                    theme: '{$theme}'
+                });
+            });
+JS;
+    }
+}
