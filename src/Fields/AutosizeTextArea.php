a609acf46c040e0e6109a8214d50c53abd234a8e
Matt Lantz <mattlantz@gmail.com>
2023-09-05 22:31:03 -0400

    because autochanging the textarea size is handy

---
+++ src/Fields/AutosizeTextArea.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Grafite\Forms\Fields;
+
+use Grafite\Forms\Fields\Field;
+
+class AutosizeTextArea extends Field
+{
+    protected static function getType()
+    {
+        return 'textarea';
+    }
+
+    protected static function getAttributes()
+    {
+        return [
+            'data-autosize',
+            'rows' => 5,
+        ];
+    }
+
+    protected static function getFactory()
+    {
+        return 'text(300)';
+    }
+
+    public static function scripts($options)
+    {
+        return [
+            '//cdn.jsdelivr.net/npm/autosize@6.0.1/dist/autosize.min.js'
+        ];
+    }
+
+    public static function onLoadJs($id, $options)
+    {
+        return '_formsjs_autosizeTextAreaField';
+    }
+
+    public static function js($id, $options)
+    {
+        return <<<JS
+                _formsjs_autosizeTextAreaField = function (element) {
+                    if (! element.getAttribute('data-formsjs-rendered')) {
+                        autosize(element);
+                    }
+                }
+            JS;
+    }
+}
