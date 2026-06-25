8bf9091476d600e76d4436c2d74f36d504675812
Matt Lantz <mattlantz@gmail.com>
2026-06-25 15:58:54 -0400

    feature: new helpers for tags field

--- src/Helpers/form.php
+++ src/Helpers/form.php
@@ -12,3 +12,18 @@ if (! function_exists('form')) {
         return app(Form::class);
     }
 }
+
+if (! function_exists('tags_values')) {
+    function tags_values(string $tags)
+    {
+        return collect(json_decode($tags))->pluck('value')->values()->toArray();
+    }
+}
+
+if (! function_exists('tags_to_string')) {
+    function tags_to_string(array $tags)
+    {
+        return collect($tags)->values()->implode(', ');
+    }
+}
+
